a62109dc51cb1502c13c1f1884d7b27eeb77b9de
leonardoz15 <leonardoz@allegheny.edu>
2018-12-17 17:42:18 -0500

    pysheets practice

--- src/main.py
+++ src/main.py
@@ -10,36 +10,35 @@ class GAPmember():
         self.net_bal = net
     #end of __init__() method
 
-
-# def updateList(self, full_list):
-#     """The updateList() method alphabetizes and updates the spreadsheet"""
-#     for self.name in full_list:
-#         name_pieces = self.name.split(" ")
-#         self.first_name = name_pieces[0] # first name
-#         self.first_name.capitilize()
-#         self.last_name = name_pieces[1]  # second name
-#         self.last_name.capitilize()
-#         self.name = self.first_name + " " + self.last_name
-#         print(self.last_name)
+#     def updateList(self, full_list):
+#         """The updateList() method alphabetizes and updates the spreadsheet"""
+#         for self.name in full_list:
+#             name_pieces = self.name.split(" ")
+#             self.first_name = name_pieces[0] # first name
+#             self.first_name.capitilize()
+#             self.last_name = name_pieces[1]  # second name
+#             self.last_name.capitilize()
+#             self.name = self.first_name + " " + self.last_name
+#             print(self.last_name)
 #
-#     full_list.sort(key = lambda x: x.self.last_name)
-#     return full_list
-    #end of updateList() function
-#end of GAPmember class
+#         full_list.sort(key = lambda x: x.self.last_name)
+#         return full_list
+#         #end of updateList() function
+# #end of GAPmember class
 
 def createList():
     """The createList function builds a list of member objects from the google sheet"""
     import operator
-    cell_list = sheet.get_row(1, returnas='cells')
+    cell_list = sheet.col(1, returnas='cells')
     rows_int = len(cell_list) - 2  #To account for top two rows
     print(rows_int)
     member_list = []
-    q = 3
+    q = 2
     for i in range(rows_int):
-        full_name = sheet[q][1]
-        email = sheet[q][2]
-        gross_bal = sheet[q][3]
-        net_bal = sheet[q][4]
+        full_name = sheet[q][0]
+        email = sheet[q][1]
+        gross_bal = sheet[q][2]
+        net_bal = sheet[q][3]
         member = GAPmember(full_name, email, gross_bal, net_bal)
         member_list.append(member)
         q + 1
@@ -60,9 +59,10 @@ def main():
     # Use creds to create a client to interact with Google Drive API
 
     print("Hello, starting out")
-
-    member_list = createList()
-    print(member_list[1], len(member_list))
+    cell_list = sheet.col(0, returnas='cell')
+    print(len(cell_list))
+    #member_list = createList()
+    #print(member_list[1], len(member_list))
     #member_list = updateList(member_list)
 
 if __name__ == '__main__':
