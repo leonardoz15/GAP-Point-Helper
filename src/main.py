1fc69d5dc3f09a1777fd5c1eb30e813901d6e1a8
leonardoz15 <leonardoz@allegheny.edu>
2019-01-28 18:22:28 -0500

    Working Build

--- src/main.py
+++ src/main.py
@@ -1,57 +1,20 @@
 #####GAP POINT HELPER#####
 #Author: Zachary Leonardo
-
-def format():
-    #Capitilizes and sorts all names by last name, then updates the sheet
-    c = 0
-    for cell in names_listcell[2:length + 2]:
-        label = cell.label
-        linked_cell = wks.cell(label)
-        name_toReplace = Name_list[c]
-        linked_cell.value = name_toReplace
-        c += 1
-#end of format()
-
-def addMember(last_list):
-    #Alphabetically add a new member
-    print("To add a new member, please specify the following: ")
-    fname_new = input("First Name:    ").lower()
-    lname_new = input("Last Name:     ").lower()
-    email_new = input("Email (@allegheny.edu):    ") + '@allegheny.edu'
-    bal_new = int(input("Starting point balance:    "))
-
-    #find where new name should be indexed
-    lname_new = lname_new.capitalize()
-    last_list.append(lname_new)
-    last_list.sort()
-    index_add = last_list.index(lname_new)
-    index_add += 2 # to account for styling
-
-    #update spreadsheet with new member
-    newmember_list = []
-    fname_new = fname_new.capitalize()
-    name_new = fname_new + " " + lname_new
-    newmember_list.extend((name_new, email_new, bal_new, bal_new))
-    wks.insert_rows(row=index_add, number=1, values=newmember_list)
-
-#end of addMember()
-
 ##############################################################################
 import pygsheets
 # Use creds to create a client to interact with Google Drive API
-client = pygsheets.authorize(service_file='client_secret.json')
+client = pygsheets.authorize(service_file='secret/client_secret.json')
 print("Welcome to the GAP Point Helper!")
-while True:
-    try:
-        name = input("What is the name of the google sheets file? ")
-        sheet = client.open(name)
-        break
-    except pygsheets.exceptions.SpreadsheetNotFound:
-        #Code to extract client email to share with
-        import json
-        jdata = json.loads(open('client_secret.json').read())
-        cl = jdata["client_email"]
-        print("Please share with the email : " + cl)
+file_name = input("What is the name of the google sheets file? ")
+try:
+    sheet = client.open(file_name)
+except pygsheets.exceptions.SpreadsheetNotFound:
+    #Code to extract client email to share with
+    import json
+    jdata = json.loads(open('client_secret.json').read())
+    cl = jdata["client_email"]
+    print("Please share with the email : " + cl)
+
 #Global variables:
 wks = sheet.sheet1
 name_list = [] #lower case list
@@ -77,15 +40,18 @@ for name in name_list:
 
 def main():
     #Main method
+    import addMember
+    import format
     #Choose which operation to perform: (Only add member at the moment)
     #TODO: add methods for adding and removing points
     while True:
         first = input("Do you need to format? This may take a few minutes for longer lists. (yes / no) ").lower() #to cut down on exec time
         if first == "yes":
-            format()
-            addMember(last_list) #Hard-coded as the only method currently
+            print("Formatting " + file_name)
+            format.formatSheet()
+            addMember.addMember(last_list) #Hard-coded as the only method currently
         elif first == "no":
-            addMember(last_list) #Hard-coded as the only method currently
+            addMember.addMember(last_list) #Hard-coded as the only method currently
         another = input("Would you like to add another? (yes / no) ").lower()
         if another == "no":
             break
